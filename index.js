0c7bdbc74bfc882eb9b6c38ab35a27a0a497c47a
Jonathan Ayala <Jonthan_Ayala@eogresources.com>
2023-08-12 12:55:50 -0700

    update stack output

--- index.js
+++ index.js
@@ -2,20 +2,40 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const dockerImage = core.getInput('dockerImage');
-  const labels = core.getInput('labels');
-  const env = core.getInput('env');
-  const overlayNetworks = core.getInput('overlayNetworks');
-  const externalNetworks = core.getInput('externalNetworks');
+  const dockerImage = core.getInput('image');
+  const environment = core.getInput('env');
+  const deployUrl = core.getInput('deployUrl');
+  const port = core.getInput('port');
 
-  console.log(dockerImage);
-  console.log(labels);
-  console.log(env);
-  console.log(overlayNetworks);
-  console.log(externalNetworks);
+  const stack = `
+    version: '3.3'  
+    services:
+      app:
+        image: ${dockerImage}
+        environment:
+          ${environment}
+        networks:
+        - net
+        - traefik-public
+        logging:
+          driver: json-file
+        deploy:
+          labels:
+            traefik.docker.network: traefik-public
+            traefik.enable: 'true'
+            traefik.http.routers.app.rule: Host(\`${deployUrl}\`)
+            traefik.http.routers.app.tls: 'true'
+            traefik.http.routers.app.tls.certresolver: leresolver
+            traefik.http.services.app.loadbalancer.server.port: '${port || 5000}'
+    networks:
+      net:
+        driver: overlay
+      traefik-public:
+        external: true
+`;
 
-  core.setOutput('dockerComposeString', 'empty');
-  console.log('empty');
+  core.setOutput('dockerComposeString', stack);
+  console.log(stack);
 } catch (error) {
   core.setFailed(error.message);
 }
